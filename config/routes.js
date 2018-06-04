ad055bcbe24e69310cc99a8fd68d6472778570ee
Nisha Bharti <me.nishabharti@gmail.com>
2018-06-04 15:23:39 -0700

    no major changes

--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,9 @@ const recipes = require("../controllers/recipes.js")
 const comments = require("../controllers/comments.js")
 
 module.exports = function(app){
-
+app.get('/', users.index);
 app.get('/users/login', users.render);
 app.post('/register', users.register);
 app.post('/login', users.login);
-app.get('/users/recipes', users.view);
+app.get('/users/recipes', recipes.view);
 }
--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -9,7 +9,7 @@ exports.seed = function(knex, Promise) {
         {name: 'Nisha', email: 'nisha@nisha.com',bio:'showing off cooking skills to the world',img_url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",password:'nisha'},
         {name: 'David', email: 'david@david.com',bio:'definitely better at cooking than coding',img_url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",password:'david'},
         {name: 'admin', email: 'admin@admin.com',bio:'admin',img_url:"",password:'admin',admin:true}
-      
+
       ]);
     });
 };
