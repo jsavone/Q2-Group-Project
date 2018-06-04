691e1333b6b57a19490e824d40a9134f463b11cd
David Sommerfeld <davidsommer9@icloud.com>
2018-06-04 14:49:24 -0700

    seeds_recipes

---
+++ db/seeds/02_recipes.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('recipes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('recipes').insert([
+        {recipe_name: "pizza", recipe_image:"",total_votes:10,ingredients:"pepperoni and cheese",directions:"roll dough, bake 400 degrees",prep_time:"15 minutes",cook_time:"30 minutes",user_id:1},
+        {recipe_name: "tacos", recipe_image:"",total_votes:20,ingredients:"cilantro and beef",directions:"roll beef in tortilla",prep_time:"5 minutes",cook_time:"15 minutes",user_id:2},
+        {recipe_name: "guacamole", recipe_image:"",total_votes:5,ingredients:"avocados,garlic, and lemons",directions:"crush avacados in bowl",prep_time:"25 minutes",cook_time:"5 minutes",user_id:3}
+        
+      ]);
+    });
+};
