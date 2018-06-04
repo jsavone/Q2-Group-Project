5ccd5260be2d650b0f2117d243baeda3e20c0083
jsavone <john.savone@gmail.com>
2018-06-04 15:23:51 -0700

    Updated index and added recipe route

--- config/routes.js
+++ config/routes.js
@@ -4,9 +4,10 @@ const recipes = require("../controllers/recipes.js")
 const comments = require("../controllers/comments.js")
 
 module.exports = function(app){
-app.get('/', users.index);  
+app.get('/', users.index);
 app.get('/users/login', users.render);
 app.post('/register', users.register);
 app.post('/login', users.login);
 
+app.get('/recipe/:id', recipes.view)
 }
---
+++ controllers/recipes.js
@@ -0,0 +1,10 @@
+const knex = require("../db/knex.js");
+
+module.exports = {
+  // CHANGE ME TO AN ACTUAL FUNCTION
+
+  view: function(req, res) {
+    res.render('user-recipe');
+  },
+
+}
