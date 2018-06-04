35870a0fec5fce9d2730a15f5e7c332a47acb2ad
jsavone <john.savone@gmail.com>
2018-06-04 14:35:52 -0700

    added .gitignore

--- controllers/users.js
+++ controllers/users.js
@@ -3,6 +3,10 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   index: function(req, res) {
-    res.send("Hello");
+    knex('recipes').join('users', 'users.id', 'recipes.user_id').then((recipes) => {
+      // res.json(recipes);
+        res.render("index", {recipes:recipes});
+    })
+
   },
 }
