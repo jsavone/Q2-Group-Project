d71755c85596cc3fa0124e0db649dfdace2e19ef
Nisha Bharti <me.nishabharti@gmail.com>
2018-06-05 20:34:05 -0700

    uploaded images of users

--- db/seeds/01_users.js
+++ db/seeds/01_users.js
@@ -5,11 +5,11 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {name: 'John', email: 'john@john.com',bio:'food and computer enthusiast',img_url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",password:'john'},
-        {name: 'Nisha', email: 'nisha@nisha.com',bio:'showing off cooking skills to the world',img_url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",password:'nisha'},
-        {name: 'David', email: 'david@david.com',bio:'definitely better at cooking than coding',img_url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",password:'david'},
+        {name: 'John', email: 'john@john.com',bio:'food and computer enthusiast',img_url:"http://www.cooking-culinary-arts-schools.org/wp-content/uploads/2013/01/Chef-holding-chicken-entree-TS.jpg",password:'john'},
+        {name: 'Nisha', email: 'nisha@nisha.com',bio:'showing off cooking skills to the world',img_url:"https://img.freepik.com/free-photo/cook-works-with-vegetables-at-kitchen_1398-370.jpg?size=338&ext=jpg",password:'nisha'},
+        {name: 'David', email: 'david@david.com',bio:'definitely better at cooking than coding',img_url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTejhJpoEuV4HcwzTbvR1Qr9McUmlfSxUZVrmjJsD6FrNW6jiKokA",password:'david'},
         {name: 'admin', email: 'admin@admin.com',bio:'admin',img_url:"",password:'admin',admin:true}
 ]);
     });
-  
+
 };
