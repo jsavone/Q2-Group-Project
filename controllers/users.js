555a9287c414fb73cde6d97f4b52595ffcb129d1
Nisha Bharti <me.nishabharti@gmail.com>
2018-06-06 10:06:21 -0700

    added hashing

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const knex = require("../db/knex.js");
+const hasher = require("../config/hasher");
 
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
@@ -28,41 +29,64 @@ module.exports = {
       })
     })
   },
+  register: (req, res) => {
+    hasher.hash(req.body).then((doctors) => {
+      knex("doctors").insert({
+        name: doctors.name,
+        email: doctors.email,
+        bio: doctors.bio,
+        img_url: doctors.img_url,
+        password: doctors.password
+      }).then(() => {
+        res.redirect('/doctors/login');
+      }).catch(() => {
+        req.session.errors.push("Register was invalid");
+      })
+    })
 
+  },
   register:function(req,res){
-    req.session.errors = null
+    req.session.errors = null;
+    hasher.hash(req.body).then((users) => {
     knex("users").insert({
-      name:req.body.name,
-      email:req.body.email,
-      bio:req.body.bio,
-      img_url:req.body.img_url,
-      password:req.body.password
+      name:users.name,
+      email:users.email,
+      bio:users.bio,
+      img_url:users.img_url,
+      password:users.password
     }).then(()=>{
       res.redirect('/users/login');
     }).catch(()=>{
       req.session.errors.push("Register was invalid");
     })
+  })
 
   },
+
+
+
+
   login:function(req,res){
     req.session.errors = null
     req.session.admin = null
     knex('users').where("email", req.body.email).then((results)=>{
       let user=results[0];
-      if(user.password===req.body.password){ //store the user id in session
-        req.session.user_id=user.id;
-        if (user.admin) {
-          req.session.admin = true;
-        }
-        req.session.save(()=>{
-          res.redirect(`/profile`);
-        })
-      }  else{
-          req.session.errors = "Email or Password was invalid";
+      hasher.check(user, req.body).then((isMatch) => {
+        if (isMatch) {
+          req.session.user_id = user.id;
+          if (user.admin) {
+            req.session.admin = true;
+          }
+          req.session.save(() => {
+            res.redirect(`/profile`);
+          })
+        } else {
+        req.session.errors = "Email or Password was invalid";
           req.session.save(()=>{
             res.redirect("/users/login");
           })
         }
+      })
       }).catch(()=>{
             req.session.errors.push("Email or Password was invalid");
             req.session.save(()=>{
