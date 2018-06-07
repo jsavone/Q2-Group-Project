68544e412a75a891859e5e374439b60b10c25968
jsavone <john.savone@gmail.com>
2018-06-07 13:09:16 -0700

    make it better

--- knexfile.js
+++ knexfile.js
@@ -14,12 +14,13 @@ module.exports = {
     },
   production: {
       client: 'pg',
-      connection: process.env.DATABASE_URL,
+      connection: process.env.postgres://xmgqbenumchuxx:1a18fcf75a3c3e3113177bce5731f8e41b5799d84d68194177c71f93fed347ed@ec2-54-235-252-23.compute-1.amazonaws.com:5432/dco385u19s7crm,
       migrations: {
           directory: __dirname + '/db/migrations',
         },
       seeds: {
-          directory: __dirname + '/db/seeds/production',
+          directory: __dirname + '/db/seeds',
+          // directory: __dirname + '/db/seeds/production',
         },
     },
 };
