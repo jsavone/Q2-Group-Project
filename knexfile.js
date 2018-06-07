eeab873e7089ac0d283bce6947ec13c3419be296
jsavone <john.savone@gmail.com>
2018-06-07 13:13:01 -0700

    make it better

--- knexfile.js
+++ knexfile.js
@@ -14,7 +14,7 @@ module.exports = {
     },
   production: {
       client: 'pg',
-      connection: process.env.postgres://xmgqbenumchuxx:1a18fcf75a3c3e3113177bce5731f8e41b5799d84d68194177c71f93fed347ed@ec2-54-235-252-23.compute-1.amazonaws.com:5432/dco385u19s7crm,
+      connection: process.env.DATABASE_URL,
       migrations: {
           directory: __dirname + '/db/migrations',
         },
