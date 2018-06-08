ee6472760915e6dfffcebb4b2f1b589cb2669dac
jsavone <john.savone@gmail.com>
2018-06-08 09:48:20 -0700

    Added Admin Seeds

--- db/seeds/04_saved_recipes.js
+++ db/seeds/04_saved_recipes.js
@@ -11,7 +11,9 @@ exports.seed = function(knex, Promise) {
         {user_id:2,recipe_id:2},
         {user_id:2,recipe_id:4},
         {user_id:3,recipe_id:3},
-        {user_id:3,recipe_id:1}
+        {user_id:3,recipe_id:1},
+        {user_id:4,recipe_id:1},
+        {user_id:4,recipe_id:2}
 
       ]);
     });
